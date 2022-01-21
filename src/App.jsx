64f1d26a9ed07cd18b5f6ca296fe45034d7eb2dc
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-01-21 16:22:23 +0530

    DESIGN PROCESS COMPLETE

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,14 @@ import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import User from "./pages/user/User";
-// import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    // <Routes>
     <>
       <Topbar />
       <div className="container">
         <Sidebar />
         <User />
-        {/* <Route path="/" element={<User />} /> */}
       </div>
-      {/* </Routes> */}
     </>
   );
 };
